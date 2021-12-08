57c230d480f8ea8607e5d4007b21c1875a9ed955
haxwithaxe <spam@haxwithaxe.net>
2021-12-08 16:52:17 -0500

    Initial commit

---
+++ tagit.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import logging
+import os
+import shutil
+# For unit tests to reference in case it changes
+from shutil import move as move_write_method
+import sys
+
+
+DEFAULT_TAG_CACHE = '.tagit.tags.cache'
+TAG_DELIM = '.'
+TAGS_PREFIX = '._tags'
+
+
+class TagitException(Exception):
+    pass
+
+
+class FailedToTag(TagitException):
+    pass
+
+
+class CacheError(TagitException):
+    pass
+
+
+def escape_tag(tag):
+    return tag.replace(' ', '_').replace(TAG_DELIM, '_')
+
+
+def load_tag_cache(cache_filename):
+    try:
+        with open(cache_filename, 'r') as cache:
+            tags_text = cache.read()
+    except FileNotFoundError:
+        return []
+    except PermissionError:
+        raise CacheError('Failed to read the tag cache from "{}" because the user does not have permission.'.format(cache_filename))
+    else:
+        return [tag for tag in tags_text.strip().split('\n') if tag]
+
+
+def write_tag_cache(tags, cache_filename):
+    existing_tags = load_tag_cache(cache_filename)
+    unique_tags = list(set([escape_tag(tag) for tag in tags] + existing_tags))
+    unique_tags.sort()
+    try:
+        with open(cache_filename, 'w') as cache:
+            cache.write('\n'.join(unique_tags))
+    except FileNotFoundError:
+        raise CacheError('Failed to cache the tags to "{}" because the directory does not exist.'.format(cache_filename))
+    except PermissionError:
+        raise CacheError('Failed to cache the tags to "{}" because the user does not have permission.'.format(cache_filename))
+
+
+def split_filename(filename):
+    name_part, ext = os.path.splitext(filename)
+    if TAGS_PREFIX in name_part:
+        name, tags_string, *_ = name_part.split(TAGS_PREFIX)
+        tags = tags_string.strip(TAG_DELIM).split(TAG_DELIM)
+    else:
+        name = name_part
+        tags = []
+    ext = ext or TAGS_PREFIX
+    return name, tags, ext
+
+
+def join_filename(name, tags, ext):
+    if not tags:
+        if ext == TAGS_PREFIX:
+            return name
+        return '{}{}'.format(name, ext)
+    tags_string = TAG_DELIM.join(tags)
+    if ext == TAGS_PREFIX:
+        return '{}{}.{}{}'.format(name, TAGS_PREFIX, tags_string, ext)
+    return '{}{}.{}{}{}'.format(name, TAGS_PREFIX, tags_string, TAGS_PREFIX,
+                                ext)
+
+
+def reconsile_tags(existing_tags, new_tags, remove_tags):
+    logging.debug('existing_tags: %s', existing_tags)
+    # Add tags
+    uniq_tags = list(set([escape_tag(tag) for tag in new_tags] + existing_tags))
+    # Remove tags
+    if remove_tags:
+        logging.debug('Hit remove')
+        for tag in [escape_tag(tag) for tag in remove_tags]:
+            try:
+                uniq_tags.pop(uniq_tags.index(tag))
+                logging.debug('Removed tag: %s', tag)
+            except (IndexError, ValueError):
+                pass
+    uniq_tags.sort()
+    return uniq_tags
+
+
+def copy_write_method(filename, new_filename):
+    name, _, ext = split_filename(filename)
+    tagged = glob.glob(join_filename(name, ['*'], ext))
+    for t in tagged:
+        if t != filename:
+            os.remove(t)
+    if filename == new_filename:
+        return
+    shutil.copy(filename, new_filename)
+
+
+def link_write_method(filename, new_filename):
+    if not os.path.exists(filename):
+        raise FileNotFoundError(filename)
+    name, _, ext = split_filename(filename)
+    tagged = glob.glob(join_filename(name, ['*'], ext))
+    for t in tagged:
+        if t != filename and os.path.islink(t):
+            os.remove(t)
+    if filename == new_filename:
+        return
+    os.symlink(os.path.abspath(filename), new_filename)
+
+
+def tag_file(filename, add_tags, remove_tags, write_method):
+    name, existing_tags, ext = split_filename(filename)
+    uniq_tags = reconsile_tags(existing_tags, add_tags, remove_tags)
+    new_filename = join_filename(name, uniq_tags, ext)
+    if new_filename == filename:
+        logging.info('No new tags: %s', add_tags)
+        return
+    try:
+        write_method(filename, new_filename)
+    except FileNotFoundError:
+        raise FailedToTag('Failed to tag "{}" because it does not exist.'.format(filename))
+    except PermissionError:
+        raise FailedToTag('Failed to tag "{}" because the user doesn\'t have permission.'.format(filename))
+    if write_method == move_write_method:
+        logging.info('Moved "%s" to "%s"', filename, new_filename)
+    else:
+        logging.info('Tagged "%s" as "%s"', filename, new_filename)
+    return True
+
+
+def tag_files(files, add_tags, remove_tags, write_method):
+    for filename in files:
+        tag_file(filename, add_tags, remove_tags, write_method)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-w', '--write-cache', action='store_true',
+                        default=False)
+    parser.add_argument('--cache', default=os.path.join(os.curdir, DEFAULT_TAG_CACHE))
+    parser.add_argument('-c', '--copy', action='store_true', default=False)
+    parser.add_argument('-l', '--link', action='store_true', default=False)
+    parser.add_argument('-m', '--move', action='store_true', default=True)
+    parser.add_argument('-f', '--files', nargs='+', default=[])
+    parser.add_argument('--log-level', type=int, choices=[
+        logging.DEBUG,
+        logging.INFO,
+        logging.WARNING,
+        logging.ERROR,
+        logging.CRITICAL
+    ], default=logging.INFO)
+    parser.add_argument('-t', '--tags', nargs='+', default=[])
+    parser.add_argument('-r', '--remove-tags', nargs='+', default=[])
+    parser.add_argument('--autocomplete', nargs='?', default=False)
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=args.log_level)
+    # Handle autocomplete
+    #  args.autocomplete can be falsey and still be present
+    if args.autocomplete is not False:
+        if isinstance(args.autocomplete, str):
+            if args.autocomplete and args.autocomplete[-1] != ' ':
+                tags = load_tag_cache(args.cache)
+                for tag in tags:
+                    if args.autocomplete in tag:
+                        print(tag)
+                return
+        print('\n'.join(load_tag_cache(args.cache)))
+        return
+
+    write_method = move_write_method
+    if args.copy:
+        write_method = copy_write_method
+    if args.link:
+        write_method = link_write_method
+
+    if not args.write_cache:
+        write_tag_cache(args.tags, args.cache)
+    tag_files(args.files, args.tags, args.remove_tags, write_method)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except TagitException as err:
+        print(err, file=sys.stderr)
+        sys.exit(1)
---
+++ test/test.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+
+import glob
+import os
+import stat
+import tempfile
+import unittest
+
+import tagit
+
+
+TEST_TAG_CACHE = 'tagit_test.tags.cache'
+TEST_INPUT_FILE = 'tagit_test.zip'
+TEST_OUTPUT_FILE = 'tagit_test._tags.bar.foo._tags.zip'
+TEST_OLD_FILE = 'tagit_test._tags.foo._tags.zip'
+MISSING_DIRECTORY = 'this directory doesn\'t exist'
+MISSING_DIRECTORY_CACHE_FILE = 'this directory doesn\'t exist/tagit_test.tags.cache'
+
+
+class _FileModTestCase(unittest.TestCase):
+
+    def _path(self, *parts):
+        return os.path.join(self.tmpdir, *parts)
+
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp(prefix='tagit_test-{}-'.format(self.__class__.__name__))
+        os.chmod(self.tmpdir, mode=stat.S_IRWXU)
+
+    def tearDown(self):
+        os.chmod(self.tmpdir, mode=stat.S_IRWXU)
+        for filename in glob.glob(self._path('*')):
+            os.remove(filename)
+        os.removedirs(self.tmpdir)
+
+
+class EscapeTag(unittest.TestCase):
+
+    def test_escapes_tag(self):
+        self.assertEqual(tagit.escape_tag('foo bar.baz'), 'foo_bar_baz')
+
+
+class SplitFilename(unittest.TestCase):
+
+    def test_split_filename_without_tags(self):
+        self.assertEqual(tagit.split_filename('tagit_test_file.zip'), ('tagit_test_file', [], '.zip'))
+
+    def test_split_filename_with_tags(self):
+        self.assertEqual(tagit.split_filename('tagit_test_file._tags.bar.foo._tags.zip'), ('tagit_test_file', ['bar', 'foo'], '.zip'))
+
+    def test_split_filename_without_tags_without_ext(self):
+        self.assertEqual(tagit.split_filename('tagit_test_file.zip'), ('tagit_test_file', [], '.zip'))
+
+    def test_split_filename_with_tags_without_ext(self):
+        self.assertEqual(tagit.split_filename('tagit_test_file._tags.bar.foo._tags'), ('tagit_test_file', ['bar', 'foo'], '._tags'))
+
+
+class JoinFilename(unittest.TestCase):
+
+    def test_join_filename_with_tags(self):
+        self.assertEqual(tagit.join_filename('tagit_test_file', ['bar', 'foo'], '.zip'), 'tagit_test_file._tags.bar.foo._tags.zip')
+
+    def test_join_filename_with_tags_without_ext(self):
+        # Missing extensions are replaced with TAGS_PREFIX in split_filename
+        self.assertEqual(tagit.join_filename('tagit_test_file', ['bar', 'foo'], '._tags'), 'tagit_test_file._tags.bar.foo._tags')
+
+    def test_join_filename_without_tags(self):
+        self.assertEqual(tagit.join_filename('tagit_test_file', [], '.zip'), 'tagit_test_file.zip')
+
+    def test_join_filename_without_tags_without_ext(self):
+        # Missing extensions are replaced with TAGS_PREFIX in split_filename
+        self.assertEqual(tagit.join_filename('tagit_test_file', [], '._tags'), 'tagit_test_file')
+
+
+class ReconsileTags(unittest.TestCase):
+
+    def test_reconsile_tags_add_tags(self):
+        self.assertEqual(tagit.reconsile_tags(['foo'], ['bar'], []), ['bar', 'foo'])
+
+    def test_reconsile_tags_remove_tags(self):
+        self.assertEqual(tagit.reconsile_tags(['foo', 'bar'], [], ['bar']), ['foo'])
+
+    def test_reconsile_tags_remove_missing_tags(self):
+        self.assertEqual(tagit.reconsile_tags(['bar', 'foo'], [], ['baz']), ['bar', 'foo'])
+
+    def test_reconsile_tags_add_and_remove_tags(self):
+        # Add 'baz' and remove 'bar'
+        self.assertEqual(tagit.reconsile_tags(['bar', 'foo'], ['baz'], ['bar']), ['baz', 'foo'])
+
+
+class LoadTagCache(_FileModTestCase):
+
+    def test_load_tag_cache_empty_cache(self):
+        open(self._path(TEST_TAG_CACHE), 'w').close()
+        self.assertEqual(tagit.load_tag_cache(self._path(TEST_TAG_CACHE)), [])
+
+    def test_load_tag_cache_full_cache(self):
+        with open(self._path(TEST_TAG_CACHE), 'w') as cache:
+            cache.write('bar\nfoo')
+        self.assertEqual(tagit.load_tag_cache(self._path(TEST_TAG_CACHE)), ['bar', 'foo'])
+
+    def test_load_tag_cache_missing_cache(self):
+        assert (not os.path.exists(self._path(TEST_TAG_CACHE)))
+        self.assertEqual(tagit.load_tag_cache(self._path(TEST_TAG_CACHE)), [])
+
+    def test_load_tag_cache_bad_permissions(self):
+        open(self._path(TEST_TAG_CACHE), 'w').close()
+        os.chmod(self._path(TEST_TAG_CACHE), mode=stat.S_IWUSR)  # Disable read access
+        with self.assertRaises(tagit.CacheError):
+            tagit.load_tag_cache(self._path(TEST_TAG_CACHE))
+
+
+class WriteTagsCache(_FileModTestCase):
+
+    def test_cache_tags_nominal(self):
+        tagit.write_tag_cache(['foo', 'bar'], self._path(TEST_TAG_CACHE))
+        with open(self._path(TEST_TAG_CACHE)) as cache:
+            output = cache.read()
+        self.assertEqual(output, 'bar\nfoo')
+
+    def test_cache_tags_bad_permissions(self):
+        open(self._path(TEST_TAG_CACHE), 'w').close()
+        os.chmod(self._path(TEST_TAG_CACHE), mode=stat.S_IRUSR)  # Disable write access
+        with self.assertRaises(tagit.CacheError):
+            tagit.write_tag_cache([], self._path(TEST_TAG_CACHE))
+
+    def text_cach_tags_missing_parent_directory(self):
+        assert not os.path.exists(MISSING_DIRECTORY)
+        with self.assertRaises(tagit.CacheError):
+            tagit.write_tag_cache([], MISSING_DIRECTORY_CACHE_FILE)
+
+
+class CopyWriteMethod(_FileModTestCase):
+
+    def test_copy_write_method_missing_file(self):
+        assert not os.path.exists(self._path(TEST_INPUT_FILE))
+        with self.assertRaises(FileNotFoundError):
+            tagit.copy_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertFalse(os.path.exists(self._path(TEST_OUTPUT_FILE)))
+
+    def test_copy_write_method_bad_permission(self):
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        open(self._path(TEST_OLD_FILE), 'w').close()
+        os.chmod(self.tmpdir, mode=stat.S_IRUSR|stat.S_IXUSR)  # Disable write access
+        with self.assertRaises(PermissionError):
+            tagit.copy_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertTrue(os.path.isfile(self._path(TEST_INPUT_FILE)))
+        self.assertTrue(os.path.isfile(self._path(TEST_OLD_FILE)))
+        self.assertFalse(os.path.exists(self._path(TEST_OUTPUT_FILE)))
+
+    def test_copy_write_method_remove_old_and_copy(self):
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        open(self._path(TEST_OLD_FILE), 'w').close()
+        tagit.copy_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertTrue(os.path.isfile(self._path(TEST_INPUT_FILE)))
+        self.assertFalse(os.path.exists(self._path(TEST_OLD_FILE)))
+        self.assertTrue(os.path.isfile(self._path(TEST_OUTPUT_FILE)))
+
+
+class LinkWriteMethod(_FileModTestCase):
+
+    def test_link_write_method_missing_file(self):
+        assert not os.path.exists(self._path(TEST_INPUT_FILE))
+        with self.assertRaises(FileNotFoundError):
+            tagit.link_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertFalse(os.path.exists(self._path(TEST_OUTPUT_FILE)))
+
+    def test_link_write_method_bad_permission(self):
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        os.symlink(self._path(TEST_INPUT_FILE), self._path(TEST_OLD_FILE))
+        os.chmod(self.tmpdir, mode=stat.S_IRUSR|stat.S_IXUSR)  # Disable write access
+        with self.assertRaises(PermissionError):
+            tagit.link_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertTrue(os.path.isfile(self._path(TEST_INPUT_FILE)))
+        self.assertTrue(os.path.islink(self._path(TEST_OLD_FILE)))
+        self.assertFalse(os.path.exists(self._path(TEST_OUTPUT_FILE)))
+
+    def test_link_write_method_remove_old_and_link(self):
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        os.symlink(self._path(TEST_INPUT_FILE), self._path(TEST_OLD_FILE))
+        tagit.link_write_method(self._path(TEST_INPUT_FILE), self._path(TEST_OUTPUT_FILE))
+        self.assertTrue(os.path.isfile(self._path(TEST_INPUT_FILE)))
+        self.assertFalse(os.path.exists(self._path(TEST_OLD_FILE)))
+        self.assertTrue(os.path.islink(self._path(TEST_OUTPUT_FILE)))
+
+
+class MoveWriteMethod(unittest.TestCase):
+    # Currently `tagit.move_write_method` is `shutil.move` so it isn't being
+    #  tested directly.
+    pass
+
+
+class TagFile(_FileModTestCase):
+
+    def test_tag_file_missing_file(self):
+        assert (not os.path.exists(self._path(TEST_INPUT_FILE)))
+        with self.assertRaises(tagit.FailedToTag):
+            tagit.tag_file(self._path(TEST_INPUT_FILE), ['foo', 'bar'], [], tagit.move_write_method)
+
+    def test_tag_file_bad_permissions(self):
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        os.chmod(self.tmpdir, mode=stat.S_IRUSR)  # Disable write access
+        with self.assertRaises(tagit.FailedToTag):
+            tagit.tag_file(self._path(TEST_INPUT_FILE), ['foo', 'bar'], [], tagit.move_write_method)
+
+    def test_tag_file_add_tags(self):
+        open(self._path('foo'), 'w').close()
+        open(self._path(TEST_INPUT_FILE), 'w').close()
+        tagit.tag_file(self._path(TEST_INPUT_FILE), ['foo', 'bar'], [], tagit.move_write_method)
+        self.assertTrue(os.path.isfile(self._path(TEST_OUTPUT_FILE)))
+        self.assertFalse(os.path.isfile(self._path(TEST_INPUT_FILE)))
+
+    def test_tag_file_remove_all_tags(self):
+        open(self._path(TEST_OUTPUT_FILE), 'w').close()
+        tagit.tag_file(self._path(TEST_OUTPUT_FILE), [], ['foo', 'bar'], tagit.move_write_method)
+        self.assertTrue(os.path.isfile(self._path(TEST_INPUT_FILE)))
+        self.assertFalse(os.path.isfile(self._path(TEST_OUTPUT_FILE)))
